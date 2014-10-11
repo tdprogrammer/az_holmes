b5c136b00437d989e6ab5c110eb7be3b29abe804
Tejas Damania <td.progammer@gmail.com>
2014-10-11 22:56:32 +0800

    fixing error

--- wrapper/server.js
+++ wrapper/server.js
@@ -2,7 +2,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write(req);
-  res.write('\n');
+  res.write('trying to get more text in it...dont know how!\n');
   res.end('Hello World\n');
 }).listen(port);
